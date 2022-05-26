52cedca6e8c3c1f8f8f7dcfc58787a0859a9cc5f
Kacper <kacperstrz@gmail.com>
2022-05-26 13:08:54 +0200

    Ukończono panel administracyjny

--- admin.php
+++ admin.php
@@ -18,7 +18,6 @@
         $value = number_format($_POST['price'], 2);
         $query = "INSERT INTO products (product_id, product, description, price) VALUES (NULL, '".$_POST['product']."', '".$_POST['description']."', ".$value.")";
         $result = $conn->query($query);
-        echo $result;
 
         $query = "SELECT product_id FROM products ORDER BY product_id DESC LIMIT 1";
         $result = $conn->query($query);
@@ -35,6 +34,70 @@
             $i++;
         }
     }
+
+    if(isset($_POST["ed"])){
+        $value = number_format($_POST['price'], 2);
+        $query = "UPDATE products SET product = '".$_POST['product']."', description = '".$_POST['description']."', price = ".$_POST['price']." WHERE product_id = ".$_POST['id'];
+        $result = $conn->query($query);
+
+        $query = "DELETE FROM product_cat WHERE product_id = ".$_POST['id'];
+        $conn->query($query);
+
+        $query = "SELECT product_id FROM products WHERE product_id =".$_POST['id'];
+        $result = $conn->query($query);
+        $id = $result->fetch_object();
+
+        $i = 1;
+        $query = "SELECT * FROM categories";
+        $result = $conn->query($query);
+        while($row = $result->fetch_object()){
+            if(isset($_POST['category'.$i])){
+                $query = "INSERT INTO product_cat (prod_cat_id, product_id, category_id) VALUES (NULL, $id->product_id, $row->category_id)";
+                $conn->query($query);
+            }
+            $i++;
+        }
+    }
+
+    if(isset($_POST['addCat'])){
+        $i = 1;
+        $query = "SELECT * FROM categories";
+        $result = $conn->query($query);
+        while($row = $result->fetch_object()){
+            if(isset($_POST['category'.$i])){
+                $query = "INSERT INTO product_cat (prod_cat_id, product_id, category_id) VALUES (NULL, ".$_POST['addCat'].", $row->category_id)";
+                $conn->query($query);
+            }
+            $i++;
+        }
+    }
+
+    if(isset($_POST['delete'])){
+        $query = "DELETE FROM product_cat WHERE product_id = ".$_POST['delete'];
+        $conn->query($query);
+        $query = "DELETE FROM products WHERE product_id = ".$_POST['delete'];
+        $conn->query($query);
+    }
+
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+    if(isset($_POST["newCat"])){
+        $query = "INSERT INTO categories (category_id, category) VALUES (NULL, '".$_POST['category']."')";
+        $result = $conn->query($query);
+    }
+
+    if(isset($_POST["edCat"])){
+        $query = "UPDATE categories SET category = '".$_POST['category']."' WHERE category_id = ".$_POST['id'];
+        $result = $conn->query($query);
+    }
+
+    if(isset($_POST['deleteCat'])){
+        $query = "DELETE FROM product_cat WHERE category_id = ".$_POST['deleteCat'];
+        $conn->query($query);
+        $query = "DELETE FROM categories WHERE category_id = ".$_POST['deleteCat'];
+        $conn->query($query);
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -83,30 +146,85 @@
             <h1>Panel administracyjny</h1>
             <?php
                 if(isset($_POST['new'])){
-                    echo "<h2><u>Utworzono przedmiot</u></h2>";
+                    echo "<h2 style='color: red;'><u>Utworzono przedmiot</u></h2>";
                 }
-            ?>
-            <h2>Dodaj przedmiot</h2>
-                <form action="admin.php" method="POST" id="addForm">
-                    <h3>Nazwa produktu</h3>
-                    <input type="text" name="product"><br>
-                    <h3>Opis produktu</h3> 
-                    <textarea name="description" form="addForm"></textarea>
-                    <h3>Cena (pln)</h3> 
-                    <input type="number" name="price" step="0.01"><br>
-                    <h3>Kategorie</h3>
-                        <?php
-                            $query = "SELECT * FROM categories";
-                            $result = $conn->query($query);
-                            $i = 1;
-                            while($row = $result->fetch_object()){
-                                echo "<input type='checkbox' name='category".$i."' value='".$row->category_id."'>".$row->category."<br>";
-                                $i++;
+                if(isset($_POST['delete'])){
+                    echo "<h2 style='color: red;'><u>Usunięto przedmiot</u></h2>";
+                }
+                if(isset($_POST['ed'])){
+                    echo "<h2 style='color: red;'><u>Zedytowano przedmiot</u></h2>";
+                }
+                if(isset($_POST['edit'])){
+                    echo "<h2>Edytuj przedmiot</h2>";
+                } else {
+                    echo "<h2>Dodaj przedmiot</h2>";
+                }
+            
+                if(isset($_POST['edit'])){
+                    $query1 = "SELECT * FROM products WHERE product_id =".$_POST['edit'];
+                    $result1 = $conn->query($query1);
+                    $row1 = $result1->fetch_object();
+                    echo "
+                    <form action='admin.php' method='POST' id='editForm'>
+                        <h3>Nazwa produktu</h3>
+                        <input type='text' name='product' value='".$row1->product."'><br>
+                        <h3>Opis produktu</h3> 
+                        <textarea name='description' form='editForm'>".$row1->description."</textarea>
+                        <h3>Cena (pln)</h3> 
+                        <input type='number' name='price' step='0.01' value='".$row1->price."'><br>
+                        <h3>Kategorie</h3>
+                    ";
+                    $query2 = "SELECT * FROM products WHERE product_id =".$_POST['edit'];
+                    $result2 = $conn->query($query2);
+                    $row2 = $result2->fetch_object();
+
+
+                    $query1 = "SELECT * FROM product_cat WHERE product_id = ".$_POST['edit'];
+                    $result1 = $conn->query($query1);
+
+                    $query = "SELECT * FROM categories";
+                    $result = $conn->query($query);
+                    $i = 1;
+                    while($row = $result->fetch_object()){
+                        echo "<input type='checkbox' name='category".$i."' value='".$row->category_id."' ";
+
+                        $query1 = "SELECT * FROM product_cat WHERE product_id = ".$_POST['edit'];
+                        $result1 = $conn->query($query1);
+                        while($row1 = $result1->fetch_object()){
+                            if($row1->category_id == $row->category_id){
+                                echo "checked";
                             }
-                            
-                        ?>
-                    <input type="submit" value="Utwórz" id="wyslij" name="new">
-                </form>
+                        }
+
+                        echo">".$row->category."<br>";
+                        $i++;
+                    }
+                    echo "<input type='submit' value='Edytuj' id='wyslij' name='ed'> 
+                    <input type='hidden' name='id' value='".$_POST['edit']."'>
+                    </form>";
+                } else {
+                    echo "
+                    <form action='admin.php' method='POST' id='addForm'>
+                        <h3>Nazwa produktu</h3>
+                        <input type='text' name='product'><br>
+                        <h3>Opis produktu</h3> 
+                        <textarea name='description' form='addForm'></textarea>
+                        <h3>Cena (pln)</h3> 
+                        <input type='number' name='price' step='0.01'><br>
+                        <h3>Kategorie</h3>
+                    ";
+
+                    $query = "SELECT * FROM categories";
+                    $result = $conn->query($query);
+                    $i = 1;
+                    while($row = $result->fetch_object()){
+                        echo "<input type='checkbox' name='category".$i."' value='".$row->category_id."'>".$row->category."<br>";
+                        $i++;
+                    }
+                    echo "<input type='submit' value='Utwórz' id='wyslij' name='new'> </form>";
+                }
+                
+            ?>
             <h2>Przedmioty</h2>
                 <table>
                     <tr>
@@ -138,9 +256,71 @@
                             
                             echo"</td>
                                 <td>
-                                    <button>Edytuj</button>
-                                    <button>Dodaj kategorię</button>
-                                    <button>Usuń</button>
+                                    <button form='addForm' type='submit' name='edit' value='".$row->product_id."'>Edytuj</button>
+                                    <button form='addForm' type='submit' name='addCat' value='".$row->product_id."'>Dodaj kategorię</button>
+                                    <button form='addForm' type='submit' name='delete' value='".$row->product_id."'>Usuń</button>
+                                </td>
+                            </tr>";
+                        }
+                        ?>
+                </table>
+
+<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+
+            <?php
+                if(isset($_POST['newCat'])){
+                    echo "<h2 style='color: red;'><u>Utworzono kategorię</u></h2>";
+                }
+                if(isset($_POST['deleteCat'])){
+                    echo "<h2 style='color: red;'><u>Usunięto kategorię</u></h2>";
+                }
+                if(isset($_POST['edCat'])){
+                    echo "<h2 style='color: red;'><u>Zedytowano kategorię</u></h2>";
+                }
+                if(isset($_POST['editCat'])){
+                    echo "<h2>Edytuj kategorię</h2>";
+                } else {
+                    echo "<h2>Dodaj kategorię</h2>";
+                }
+            
+                if(isset($_POST['editCat'])){
+                    $query1 = "SELECT * FROM categories WHERE category_id =".$_POST['editCat'];
+                    $result1 = $conn->query($query1);
+                    $row1 = $result1->fetch_object();
+                    echo "
+                    <form action='admin.php' method='POST' id='editCForm'>
+                        <h3>Nazwa produktu</h3>
+                        <input type='text' name='category' value='".$row1->category."'><br>
+                        <input type='submit' value='Edytuj' id='wyslij' name='edCat'> 
+                        <input type='hidden' name='id' value='".$_POST['editCat']."'>
+                    </form>";
+                } else {
+                    echo "
+                    <form action='admin.php' method='POST' id='addCForm'>
+                        <h3>Nazwa produktu</h3>
+                        <input type='text' name='category'><br>
+                        <input type='submit' value='Utwórz' id='wyslij' name='newCat'> 
+                    </form>";
+                }
+                
+            ?>
+            <h2>Przedmioty</h2>
+                <table>
+                    <tr>
+                        <th>Kategoria</th>
+                        <th>Opcje</th>
+                    </tr>
+                        <?php
+                        
+                        $query = "SELECT * FROM categories";
+                        $result = $conn->query($query);
+
+                        while($row = $result->fetch_object()){
+                            echo "<tr>
+                                <td>".$row->category."</td>
+                                <td>
+                                    <button form='addCForm' type='submit' name='editCat' value='".$row->category_id."'>Edytuj</button>
+                                    <button form='addCForm' type='submit' name='deleteCat' value='".$row->category_id."'>Usuń</button>
                                 </td>
                             </tr>";
                         }
